081b6576baa753cf98027e564e31b69eff43b819
AlamCordoba <cordobaalam90@gmail.com>
2025-03-06 23:17:08 -0600

    Code: Add new slider

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,4 @@
-//const rectangulo = document.querySelector('.rectangulo-expandible');
-//const contenido = document.querySelector('.rectangulo-expandible .contenido');
-
-//const observer = new IntersectionObserver(entries => {
-    //entries.forEach(entry => {
-        //if (entry.isIntersecting) {
-            // El rectángulo es visible
-           // rectangulo.classList.add('rectangulo-expandido');
-           // contenido.style.opacity = '1';
-        //} else {
-            // El rectángulo no es visible
-            //rectangulo.classList.remove('rectangulo-expandido');
-            //contenido.style.opacity = '0';
-        //}
-    //});
-//}, { threshold: 1 }); // threshold: 1 significa que el elemento debe ser 100% visible
-
-//observer.observe(rectangulo);
-
+//Función para la animación del rectangulo amarillo
 document.addEventListener("DOMContentLoaded", function () {
     const rectangle = document.querySelector(".rectangle");
 
@@ -30,3 +12,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
     observer.observe(rectangle);
 });
+
+//Función para la aparicion de texto del slider
+document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('click', function(event) {
+
+        let parentDiv = this.parentElement;
+        let titleElement = parentDiv.querySelector('.hidden-title'); 
+        let textElement = parentDiv.querySelector('.hidden-text'); 
+        let titleContainer = document.getElementById("infoTitle");
+        let textContainer = document.getElementById("infoText");
+        let textDiv = document.querySelector(".text-container");
+
+        if (titleElement && textElement) {
+            titleContainer.textContent = titleElement.textContent;
+            textContainer.textContent = textElement.textContent;
+            textDiv.classList.add("show");
+        }
+    });
+});
+
+//Funcion para la aparición del rectangulo amarillo inferior
+
+
+
+
+
+
+
+
+
