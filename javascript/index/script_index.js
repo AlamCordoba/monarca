c7e8dd92712aa00bf978aeef77212ec48bcf1621
Omar David Conde Suarez <103803283+OmarUV11@users.noreply.github.com>
2025-05-10 22:55:49 -0600

    Ajuste del boton de preproducción del video
    Se agrego el JS Del botón para que se inicie el video de la pagina , así como también cuando termine se pueda volver a repetir otra vez 
    
    Faltan los ajuste de medias

--- javascript/index/script_index.js
+++ javascript/index/script_index.js
@@ -1,10 +1,24 @@
-function playPause(btn,vid){
-	var vid = document.getElementById(vid);
-	if(vid.paused){
+function playPause(btn, vidId) {
+	var vid = document.getElementById(vidId);
+
+	// Agrega este bloque una sola vez para evitar múltiples registros del evento
+	if (!vid.hasAttribute('data-ended-listener')) {
+		vid.addEventListener('ended', function() {
+			console.log("El video ha terminado");
+			vid.currentTime = 0;
+			btn.innerHTML = "Conoce Monarca";
+			btn.style.display = "block";
+		});
+		vid.setAttribute('data-ended-listener', 'true'); // Para no volver a añadir el evento
+	}
+
+	if (vid.paused) {
 		vid.play();
 		btn.innerHTML = "Pause";
+		btn.style.display = "none";
 	} else {
 		vid.pause();
 		btn.innerHTML = "Conoce Monarca";
+		btn.style.display = "block";
 	}
 }
